cc2657f9dd0055de1a24806a722524221f65915f
yind7510 <yind7510@CA-SU-F106-36.sduhsd.lan>
2020-01-31 10:43:06 -0800

    hi

--- APCSA/src/Unit0/Input.java
+++ APCSA/src/Unit0/Input.java
@@ -22,20 +22,39 @@ public class Input
 		short shortOne, shortTwo;
 
 
-		System.out.print("Enter an integer :: ");
+		System.out.print("Enter an integer :: 2 ");
 		intOne = keyboard.nextInt();
 
-
-		System.out.print("Enter an integer :: ");
+		System.out.print("Enter an integer :: 3 ");
 		intTwo = keyboard.nextInt();
+		
+		System.out.print("Enter a double :: 2.5 ");
+		doubleOne = keyboard.nextDouble();
+		
+		System.out.print("Enter a double :: 3.5 ");
+		doubleTwo = keyboard.nextDouble();
+		
+		System.out.print("Enter a float :: 6 ");
+		floatOne = keyboard.nextFloat();
+		
+		System.out.print("Enter a float :: 7 ");
+		floatTwo = keyboard.nextFloat();
+		
+		System.out.print("Enter a short :: 8 ");
+		shortOne = keyboard.nextShort();
+		
+		System.out.print("Enter a short :: 9 ");
+		shortTwo = keyboard.nextShort();
 
 
 		//add in input for all variables
 
 
-		System.out.println();
+
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
+		System.out.println("double one = " + doubleOne);
+		
 
 		//add in output for all variables
 
