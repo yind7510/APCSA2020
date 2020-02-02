5020ef9677da7d3b8a10403005e47c887790cc76
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-02 15:02:55 -0800

    Update Variables.java

--- APCSA/src/Unit0/Variables.java
+++ APCSA/src/Unit0/Variables.java
@@ -1,41 +1,106 @@
-package Unit0;
+package unit0;
+
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+
+
+//Name - Derek Yin
+
+//Date - 2/2/2020
+
+//Class - APCSA
+
+//Lab  - Variables
+
+
 
 public class Variables
+
 {
+
 	public static void main ( String[] args )
+
 	{
+
 		//define 1 variable of each of the
+
 		//following data types
+
 		//byte		short		int 		long
+
 		//float		double
+
 		//char      boolean		String
 
+
+
 		//integer variables
+
 		byte byteOne = 127;
+		short shortOne = -1;
+		int intOne = 1;
+		long longOne = 1111111;
+		float floatOne = 1/3;
+		double doubleOne = 1.521;
+		char charOne = 'A';
+		boolean booleanOne = true;
+		String StringOne = "hello world";
+				
+
+
 
 
 		//decimal variables
 
 
+
+
+
 		//other integer types
+
+
+
 
 
 		//other types
 
 
+
+
+
 		//output your information here
+
 		System.out.println("/////////////////////////////////");
+
 		System.out.println("*                               *");
+
 		System.out.println("*        integer types          *");
+
 		System.out.println("*                               *");
+
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*StringOne = "+StringOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -49,4 +114,5 @@ public class Variables
 
 
 	}
+
 }
