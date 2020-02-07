6c8a480516ee36988cbc5abba3d8ca9432f07b35
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-07 10:31:23 -0800

    Add files via upload

---
+++ APCSA/Cube.java
@@ -0,0 +1,13 @@
+package Unit2;
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Derek Yin
+//Date - 2/3/2020
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return 6*side*side;
+	}
+}
