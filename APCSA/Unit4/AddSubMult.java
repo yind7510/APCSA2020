f552ae10c9032bd549a7fa9b3d0263e490dfc4d3
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-10 21:18:52 -0800

    Create AddSubMult.java

---
+++ APCSA/Unit4/AddSubMult.java
@@ -0,0 +1,45 @@
+package Unit4;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name -
+
+
+
+import static java.lang.System.*; 
+
+import java.util.Scanner;
+
+
+
+public class AddSubMult
+
+{
+
+	public static double check( double a, double b )
+
+	{
+
+		if (a>b) {
+
+			return a-b;
+
+		}
+
+		if (b>a) {
+
+			return b-a;
+
+		}
+
+		else { //a=b
+
+			return a*b;
+
+		}
+
+	}
+
+}
