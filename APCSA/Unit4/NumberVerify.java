60db8de0472ad5af1f596ca4a584cde4999eb432
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-10 21:17:45 -0800

    Create NumberVerify.java

---
+++ APCSA/Unit4/NumberVerify.java
@@ -0,0 +1,65 @@
+package Unit4;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+
+
+//Name - Derek Yin
+
+//Date -  
+
+//Class -
+
+//Lab  -
+
+
+
+import static java.lang.System.*;
+
+
+
+public class NumberVerify
+
+{
+
+	public static boolean isOdd( int num )
+
+	{
+
+		if (num % 2 != 0) {
+
+			return true;
+
+		}
+
+		else {
+
+			return false;
+
+		}
+
+	}
+
+	
+
+	public static boolean isEven( int num )
+
+	{
+
+		if (num % 2 == 0) {
+
+			return true;
+
+		}
+
+		else {
+
+			return false;
+
+		}
+
+	}	
+
+}
