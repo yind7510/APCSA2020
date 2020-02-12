399df282969a5708c5cc1a39f60a553578b11ccb
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-11 20:05:19 -0800

    Create AvgLen.java

---
+++ APCSA/Unit5/AvgLen.java
@@ -0,0 +1,25 @@
+package Unit5;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name -
+
+//Date -
+
+
+
+public class AvgLen
+
+{
+
+   public static double go( String a, String b )
+
+	{
+
+		return (double) (a.length() + b.length()) / 2;
+
+	}
+
+}
