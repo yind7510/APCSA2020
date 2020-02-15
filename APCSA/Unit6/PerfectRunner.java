5d01962bd2ebe3b2f0f0c747702ff6c5ad991b9d
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-15 15:49:54 -0800

    Create PerfectRunner.java

---
+++ APCSA/Unit6/PerfectRunner.java
@@ -0,0 +1,83 @@
+package Unit6;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name -
+
+
+
+import static java.lang.System.*; 
+
+
+
+public class PerfectRunner
+
+{
+
+	public static void main( String args[] )
+
+	{
+
+		Perfect p = new Perfect(496);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(45);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(6);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(14);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(8128);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(1245);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(33);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(28);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(27);
+
+		System.out.println(p);
+
+		
+
+		p.setNumber(33550336);
+
+		System.out.println(p);						
+
+	}
+
+}
