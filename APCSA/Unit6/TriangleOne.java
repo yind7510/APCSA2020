9899d6a9f6f47d7949b2ead29820f87d512f9870
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-15 15:55:12 -0800

    Create TriangleOne.java

---
+++ APCSA/Unit6/TriangleOne.java
@@ -0,0 +1,67 @@
+package Unit6;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name
+
+
+
+import static java.lang.System.*;
+
+
+
+public class TriangleOne
+
+{
+
+	private String word;
+
+
+
+	public TriangleOne()
+
+	{
+
+		word="chris";
+
+	}
+
+
+
+	public TriangleOne(String s)
+
+	{
+
+		setWord(s);
+
+	}
+
+
+
+	public void setWord(String s)
+
+	{
+
+		word = s;
+
+	}
+
+
+
+	public void print( )
+
+	{
+
+		for (int i=word.length(); i>0; i--) {
+
+			System.out.println(word.substring(0,i));
+
+		}
+
+		System.out.println("\n");
+
+	}
+
+}
