91eb37aa248c375b0c6c6207ff7cadca28f1d9ef
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-15 17:29:10 -0800

    Create WordRunner.java

---
+++ APCSA/Unit6/WordRunner.java
@@ -0,0 +1,55 @@
+package Unit6;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name
+
+
+
+import static java.lang.System.*;
+
+
+
+public class WordRunner
+
+{
+
+	public static void main ( String[] args )
+
+	{
+
+		Word w = new Word("Hello");
+
+		System.out.println(w);
+
+		
+
+		w.setString("World");
+
+		System.out.println(w);
+
+		
+
+		w.setString("JukeBox");
+
+		System.out.println(w);
+
+
+
+		w.setString("TCEA");
+
+		System.out.println(w);
+
+
+
+		w.setString("UIL");
+
+		System.out.println(w);
+
+
+
+	}
+
+}
