66c119c28ab97f5a5affdbb0eeedb2c3c2b17b49
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-02-15 16:38:13 -0800

    Create TriangleOneRunner.java

---
+++ APCSA/Unit6/TriangleOneRunner.java
@@ -0,0 +1,45 @@
+package Unit6;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name
+
+
+
+import static java.lang.System.*;
+
+
+
+public class TriangleOneRunner
+
+{
+
+	public static void main ( String[] args )
+
+	{
+
+		TriangleOne t = new TriangleOne("hippo");
+
+		t.print();
+
+		t.setWord("abcd");
+
+		t.print();
+
+		t.setWord("it");		
+
+		t.print();
+
+		t.setWord("a");		
+
+		t.print();
+
+		t.setWord("chicken");		
+
+		t.print();
+
+	}
+
+}
