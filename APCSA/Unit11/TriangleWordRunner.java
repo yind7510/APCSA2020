d9134bd4d3a70b1cc06aeefa3822af2afed4e149
yind7510 <33039104+yind7510@users.noreply.github.com>
2020-03-06 17:39:18 -0800

    Create TriangleWordRunner.java

---
+++ APCSA/Unit11/TriangleWordRunner.java
@@ -0,0 +1,49 @@
+package Unit11;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name -
+
+
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+
+
+public class TriangleWordRunner
+
+{
+
+	public static void main( String args[] )
+
+	{
+
+   	Scanner keyboard = new Scanner(System.in);
+
+   	String choice="";
+
+		do{
+
+			out.print("\nEnter a word :: ");
+
+			String word = keyboard.next();	
+
+			
+
+			TriangleWord.printTriangle(word);
+
+
+
+			System.out.print("\nDo you want to enter more sample input? ");
+
+			choice=keyboard.next();			
+
+		}while(choice.equals("Y")||choice.equals("y"));		
+
+	}
+
+}
